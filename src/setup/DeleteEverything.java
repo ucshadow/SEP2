d3fe543a2f6fd5ef00b2b1adaa622022d3c4c1b3
NeK000 <260063@via.dk>
2017-12-13 11:07:59 +0100

    Cancel button removed user info

---
+++ src/setup/DeleteEverything.java
@@ -0,0 +1,20 @@
+package setup;
+
+import java.util.Scanner;
+
+public class DeleteEverything {
+    public static void main(String[] args) {
+        String username;
+        String pass;
+        System.out.println("Enter username for postgres");
+        username = new Scanner(System.in).nextLine();
+        System.out.println("Enter password for postgres");
+        pass = new Scanner(System.in).nextLine();
+        System.out.println("Database will be deleted in just a minute");
+        Database database = new Database(username, pass);
+        database.dropAll();
+        database.deleteFile("postgresUsernameAndPass.txt");
+        System.gc();
+
+    }
+}
